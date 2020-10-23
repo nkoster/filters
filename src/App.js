a4de666b536c3a2c43d8f56882475c9a7d475d55
Niels Koster <niels.koster@portavita.nl>
2020-10-23 11:30:54 +0200

    Use mock API with delay and spinner

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import './App.css';
-import FilterMenu from './components/FilterMenu';
-import TankList from './components/TankList';
-import list from './Tanks'
-
-const items = [
-  {
-    filter: 'color',
-    menu: [
-      'red', 'green', 'blue'
-    ]
-  },
-  {
-    filter: 'price',
-    menu: [
-      '10', '20', '30'
-    ]
-  }
-]
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import FilterMenu from './components/FilterMenu'
+import testApi from './api'
+import { CircularProgress } from '@material-ui/core'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [list, setList] = useState([])
+  useEffect(_ => {
+    testApi.fetchData((error, data) => {
+        if (error) {
+            setIsLoading(false)
+            return console.error(error)
+        }
+        setList(data)
+        setIsLoading(false)
+    })
+  }, [])
   return (
     <div className='App'>
       <header className='App-header'>
-        <FilterMenu list={list} />
+        {isLoading ? <CircularProgress /> : <FilterMenu list={list} />}
       </header>
     </div>
   )
